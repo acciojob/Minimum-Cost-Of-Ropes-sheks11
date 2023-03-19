745303a9e6b89f03d2a78fb60f1b6008c24a1b08
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-03-19 19:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  var input=document.querySelector("#rope-lengths").value;
+  var ropes=input.split(",");
+  ropes.sort(function (a,b) {
+	  return a-b;	
+  });
+  var totalCost=0;
+	var cost=0;
+	while(ropes.length>1){
+		cost=ropes[0]+ropes[1];
+		totalCost+=cost;
+		ropes.splice(0,2);
+		ropes.push(cost);
+		ropes.sort(function (a,b) {
+	  return a-b	
+  });
+	}
+ document.querySelector("#rope-lengths").textContent=totalCost;
+ 
 }  
